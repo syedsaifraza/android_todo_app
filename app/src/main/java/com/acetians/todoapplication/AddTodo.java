94682f779f269deb20f8d19cff0a5dacd736168d
Syed Saif Raza <syedsaifrza@gmail.com>
2024-08-02 21:26:26 +0530

    Navigation Working & Simple UI Workding

--- app/src/main/java/com/acetians/todoapplication/AddTodo.java
+++ app/src/main/java/com/acetians/todoapplication/AddTodo.java
@@ -1,6 +1,10 @@
 package com.acetians.todoapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -9,12 +13,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AddTodo extends AppCompatActivity {
+public class AddTodo extends AppCompatActivity implements View.OnClickListener{
+    Button viewTodosBtn;
+    Button saveTodo;
+    EditText inputTitle;
+    EditText inputDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_page);
-        Toast.makeText(this,"Saif ",Toast.LENGTH_LONG).show();
 
+        viewTodosBtn = findViewById(R.id.view_todo_btn);
+
+        viewTodosBtn.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.view_todo_btn){
+            Intent i = new Intent(AddTodo.this,MainActivity.class);
+            startActivity(i);
+        }
+    }
+    public void showToast(String message){
+     Toast.makeText(this,message,Toast.LENGTH_LONG ).show();
     }
 }
--- app/src/main/java/com/acetians/todoapplication/MainActivity.java
+++ app/src/main/java/com/acetians/todoapplication/MainActivity.java
@@ -3,7 +3,10 @@ package com.acetians.todoapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,21 +18,38 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
 
     Button navigateButton;
+    public  String[] todos = {"Work A","Work B","Work C","Work D"};
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-   navigateButton=findViewById(R.id.btn_add_todo);
-   navigateButton.setOnClickListener(this);
+        ListView listView = new ListView(this);
+
+        // Create an ArrayAdapter using the array and a default spinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1, // Layout for each item
+                todos // Data to display
+        );
+
+        // Set the adapter to the ListView
+        listView.setAdapter(adapter);
+        LinearLayout layout = findViewById(R.id.list_layout);
+        // Add ListView to the parent layout
+        layout.addView(listView);
+       navigateButton=findViewById(R.id.btn_add_todo);
+       navigateButton.setOnClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View view) {
-        showToast("Showing sample toast here");
 
-        Intent i = new Intent(this, AddTodo.class);
+        Intent i = new Intent(MainActivity.this, AddTodo.class);
         startActivity(i);
     }
 
